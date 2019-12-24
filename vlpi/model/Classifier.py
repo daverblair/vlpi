Added KL annealing, fixed one cyle inference

vlpi/model/Classifier.py
@@ -6,27 +6,22 @@ Created on Wed Sep  4 14:51:54 2019
 @author: davidblair
 """
 import torch
-import torch.nn as nn
 import pyro
+import torch.nn as nn
 import pyro.distributions as dist
 from typing import Iterable
-from vlpi.Decoders import LinearDecoder,LinearDecoder_Monotonic,NonlinearMLPDecoder,NonlinearMLPDecoder_Monotonic
-from vlpi.utils import random_catcov,infer_beta_from_CI
+from vlpi.model.Decoders import  LinearDecoder_Monotonic,LinearDecoder,NonlinearMLPDecoder,NonlinearMLPDecoder_Monotonic
+from vlpi.utils.UtilityFunctions import random_catcov
 import copy
 
-class DiscriminativeModel(nn.Module):
+class Classifier(nn.Module):
 
     def SwitchDevice(self,new_compute_device):
         self.compute_device=new_compute_device
         self.to(self.compute_device)
         
-    def _returnUpdatingParamsFlattened(self,withPosterior=True,withEncoder=True):
-        all_params=[]
-        for p_vals in self.decoder.parameters():
-            all_params+=[p_vals.detach().flatten()]
-        return torch.cat(all_params)
 
-    def __init__(self, numObsTraits:int, numCatList:Iterable[int],mappingFunction:str,linkFunction:str,**kwargs):
+    def __init__(self, numObsTraits:int, numCatList:Iterable[int],decoderType:str,**kwargs):
 
 
         """
@@ -35,32 +30,44 @@ class DiscriminativeModel(nn.Module):
         
         numObsTraits-->number of phenotypes included into the model
         numCatList-->List of the number of categories for each categorical covariate; total # of categorical covariates = len(numCatList).
-        mappingFunction-->type of function mapping observed to latent phenotype space. Options include: Linear, Linear_Monotonic, Nonlinear, Nonlinear_Monotonic.
-        linkFunction-->function used to link continuous prediction scores to expectation of binary RV. Choices are ['Logit','Probit']
+        decoderType-->type of function mapping observed to latent phenotype space. Options include: Linear, Linear_Monotonic, Nonlinear, Nonlinear_Monotonic.
         
         **kwargs: There are multiple keyword arguments available as well. These handle many of the hyperparameters and more esoteric modeling options. Information regarding each is provided below
         """
 
-        super(DiscriminativeModel,self).__init__()
+        super(Classifier,self).__init__()
         self.numObsTraits=numObsTraits
         self.numCatList=numCatList
-        self.mappingFunction=mappingFunction
+        self.decoderType=decoderType
 
-        assert self.mappingFunction in ['Linear','Linear_Monotonic','Nonlinear','Nonlinear_Monotonic'], "Only Linear, Linear_Monotonic, Nonlinear, and Nonlinear_Monotonic models are supported by the DiscriminativeModel"
+        assert self.decoderType in ['Linear','Linear_Monotonic','Nonlinear','Nonlinear_Monotonic'], "Only Linear, Linear_Monotonic, Nonlinear, and Nonlinear_Monotonic models are supported by the DiscriminativeModel"
         #kwargs
+        
         allKeywordArgs = list(kwargs.keys())
+        
+        
+        if 'linkFunction' not in allKeywordArgs:
+            self.linkFunction = lambda x:torch.sigmoid(x)
+        else:
+            linkFunction = kwargs['linkFunction']
+            assert linkFunction in ['Logit','Probit'],"Only Logit and Probit link functions currently supported."
+            
+            if linkFunction=='Logit':
+                self.linkFunction = lambda x:torch.sigmoid(x)
+            else:
+                self.linkFunction = lambda x:dist.Normal(0.0,1.0).cdf(x)
         
         if 'computeDevice' not in allKeywordArgs:
             """
             Specifies compute device for model fitting, can also be specified later by
             calling SwitchDevice
-        
+
             """
             self.compute_device=None
         else:
             self.compute_device=kwargs['computeDevice']
-
-
+        
+        
         if 'dropLinearCovariateColumn' not in allKeywordArgs:
             """
             Specifies model to drop one category from each covariate. Defaults to True.
@@ -69,79 +76,48 @@ class DiscriminativeModel(nn.Module):
         else:
             self.dropLinearCovariateColumn=kwargs['dropLinearCovariateColumn']
             assert isinstance(self.dropLinearCovariateColumn,bool),"dropLinearCovariateColumn expects boolean value"
-
+            
         if 'coupleCovariates' not in allKeywordArgs:
             """
-            Specifies whether to couple covariate variables into Nonlinear NN mapping function. Defaults to True for
-            Discriminative model
+            Specifies whether to couple covariates to non-linear MLP network (True), or to model them  using an independent linear network (False). Defaults to True.
             """
-            self.coupleCovariates=False
+            self.coupleCovariates=True
         else:
             self.coupleCovariates=kwargs['coupleCovariates']
-            assert isinstance(self.coupleCovariates,bool),"coupleCovariates expects boolean value"
+            assert isinstance(self.dropLinearCovariateColumn,bool),"coupleCovariates expects boolean value"
+            if self.decoderType not in ['Nonlinear']:
+                print("Warning: Not fitting Nonlinear model. Coupling covariates has no effect on inference.")
+        
+
             
-        if 'neuralNetworkHyperparameters' not in allKeywordArgs:
-            self.decoderHyperparameters={'n_layers' : 2, 'n_hidden' : 64, 'dropout_rate': 0.5, 'use_batch_norm':True}
+        if 'decoderNetworkHyperparameters' not in allKeywordArgs:
+            self.decoderHyperparameters={'n_layers' : 2, 'n_hidden' : 32, 'dropout_rate': 0.1, 'use_batch_norm':True}
+
         else:
-            self.decoderHyperparameters=kwargs['neuralNetworkHyperparameters']
-            assert isinstance(self.decoderHyperparameters,dict),"Expects dictionary of decoder hyperparameters"
-            assert set(self.decoderHyperparameters.keys())==set(['n_layers','n_hidden','dropout_rate','use_batch_norm']),"Encoder hyperparameters must include: 'n_layers','n_hidden','dropout_rate','use_batch_norm'"
-
-
-
-        if self.mappingFunction=='Linear':
-            self.decoder = LinearDecoder(self.numObsTraits,self.numCatList,1,self.dropLinearCovariateColumn,True)
-        elif self.mappingFunction=='Linear_Monotonic':
-            self.decoder=LinearDecoder_Monotonic(self.numObsTraits,self.numCatList,1,self.dropLinearCovariateColumn,True)
-        elif self.mappingFunction=='Nonlinear':
-            self.decoder=NonlinearMLPDecoder(self.numObsTraits,self.numCatList,1,self.dropLinearCovariateColumn,self.coupleCovariates,**self.decoderHyperparameters)
-        elif self.mappingFunction=='Nonlinear_Monotonic':
-            self.decoder=NonlinearMLPDecoder_Monotonic(self.numObsTraits,self.numCatList,1,self.dropLinearCovariateColumn,**self.decoderHyperparameters)
-
+            self.decoderHyperparameters = kwargs['decoderNetworkHyperparameters']
+            assert isinstance(self.encoderHyperparameters,dict),"Expects dictionary of decoder hyperparameters"
+            assert set(self.encoderHyperparameters.keys())==set(['n_layers','n_hidden','dropout_rate','use_batch_norm']),"Decoder hyperparameters must include: 'n_layers','n_hidden','dropout_rate','use_batch_norm'"
+            
+            if self.decoderType not in ['Nonlinear','Nonlinear_Monotonic']:
+                print("Warning: Decoder neural network hyperparameters specified for a linear model. Parameters will not be used.")
+        
         if self.dropLinearCovariateColumn:
             self.numCovParam = sum(self.numCatList)-len(self.numCatList)
         else:
             self.numCovParam = sum(self.numCatList)
-        
-        self.linkFunctionType = linkFunction
-        if self.linkFunctionType=='Probit':
-            self.linkFunction = lambda x:dist.Normal(0.0,1.0).cdf(x)
+                    
+        if self.decoderType=='Linear':
+            self.decoder = LinearDecoder(self.numObsTraits,self.numCatList,1,self.dropLinearCovariateColumn,True)
+        elif self.decoderType=='Linear_Monotonic':
+            self.decoder = LinearDecoder_Monotonic(self.numObsTraits,self.numCatList,1,self.dropLinearCovariateColumn,True)
+        elif self.decoderType == 'Nonlinear':
+            self.decoder = NonlinearMLPDecoder(self.numObsTraits,self.numCatList,1,self.dropLinearCovariateColumn,self.coupleCovariates,**self.decoderHyperparameters)
         else:
-            self.linkFunction = lambda x:torch.sigmoid(x)
-            
-        self.to(self.compute_device)
+            self.decoder = NonlinearMLPDecoder_Monotonic(self.numObsTraits,self.numCatList,1,self.dropLinearCovariateColumn,**self.decoderHyperparameters)
+        
+        if self.compute_device is not None:
+            self.SwitchDevice(self.compute_device)
         self.eval()
-
-    def SimulateData(self,numSamples,obsDisPrevalencePrior95_CI = [0.1,0.25]):
-
-        """
-        Draws numSamples of samples from model.
-        numSamples: number of samples
-        obsDisPrevalencePrior95_CI: 95%CI for the generating distribution from which to draw the observed disease prevalence rates.
-        """
-
-        cat_cov_list = []
-        for n_cat in self.numCatList:
-            cat_cov_list+=[random_catcov(n_cat,numSamples,self.compute_device)]
-
-        betaPrevPrior=torch.tensor(infer_beta_from_CI(obsDisPrevalencePrior95_CI),dtype=torch.float32,device=self.compute_device)
-        prevRates = dist.Beta(betaPrevPrior[0]*torch.ones(1,self.numObsTraits,dtype=torch.float32,device=self.compute_device),betaPrevPrior[1]*torch.ones(1,self.numObsTraits,dtype=torch.float32,device=self.compute_device)).sample()
-        obs_dis_data = dist.Bernoulli(torch.ones(numSamples,1,dtype=torch.float32,device=self.compute_device)*prevRates).sample()
-
-
-        dis_liability = self.decoder(obs_dis_data,*cat_cov_list)
-        anchor_dx_prob =  self.linkFunction(dis_liability)
-        anchor_dx=dist.Bernoulli(anchor_dx_prob).sample()
-        
-        outputDict={}
-        outputDict['incidence_data']=obs_dis_data
-        if self.numCovParam>0:
-            outputDict['covariate_data']=cat_cov_list
-        else:
-            outputDict['covariate_data']=[]
-        outputDict['anchor_dx_data'] = anchor_dx
-    
-        return outputDict
 
     def model(self,obs_data=None, cat_cov_list=None,anchor_dx=None,sample_scores=None,minibatch_scale=1.0):
         assert obs_data is not None,"Model requires input data. To simulate random samples, use SimulateData function."
@@ -162,7 +138,7 @@ class DiscriminativeModel(nn.Module):
         assert anchor_dx.shape[0]==obs_data.shape[0],"Dimensions of anchor and observed dx data must match."
 
 
-    def PredictAnchorDx(self,dis_array,covariate_list):
+    def PredictLabels(self,dis_array,covariate_list):
         """
         Returns expected value for probability of having anchor disease
         """
@@ -172,9 +148,7 @@ class DiscriminativeModel(nn.Module):
 
     def PackageCurrentState(self):
         packaged_model_state={}
-        packaged_model_state['model_state'] = copy.deepcopy(self.state_dict())
-        packaged_model_state['posterior_params'] = None
-        packaged_model_state['prior_params'] = None
+        packaged_model_state['model_state'] = copy.deepcopy(self.state_dict(keep_vars=True))
         return packaged_model_state
 
     def LoadPriorState(self,prior_model_state):

vlpi/model/VAE.py
@@ -25,7 +25,7 @@ class VAE(nn.Module):
         self.compute_device=new_compute_device
         self.to(self.compute_device)
         
-    def _encoder_only_model(self,obs_data=None, cat_cov_list=None,label_dx=None, sample_scores=None,numSamples=None,minibatch_scale=1.0):
+    def _encoder_only_model(self,obs_data=None, cat_cov_list=None,label_dx=None, sample_scores=None,numSamples=None,minibatch_scale=1.0,annealing_factor=1.0):
         
         
         assert sample_scores is not None, "Sample scores must be included in order to train the encoder individually."
@@ -36,13 +36,14 @@ class VAE(nn.Module):
 
         latent_vals = torch.zeros(sample_scores.shape,dtype=torch.float32,device=sample_scores.device)
         norm_vals = torch.arange(sample_scores.shape[0],device=sample_scores.device).to(dtype=torch.float32)
-        norm_vals = dist.Normal(0.0,1.0).icdf(1.0-(norm_vals+1.0)/(norm_vals.shape[0]+1.0))
+        
+        norm_vals = dist.Normal(torch.tensor(0.0,device=sample_scores.device,dtype=torch.float32),torch.tensor(1.0,device=sample_scores.device,dtype=torch.float32)).icdf(1.0-(norm_vals+1.0)/(norm_vals.shape[0]+1.0))
         for i in range(self.nLatentDim):
             rankings=sample_scores[:,i].argsort(dim=0,descending=True)
             latent_vals[rankings,i]=norm_vals
 
         #add noise to latent states to prevent overtraining the variance
-        latent_vals+=dist.Normal(0.0,1.0).sample(latent_vals.shape)
+        latent_vals+=dist.Normal(torch.tensor(0.0,dtype=torch.float32,device=sample_scores.device),torch.tensor(1.0,dtype=torch.float32,device =sample_scores.device)).sample(latent_vals.shape)
 
         with pyro.poutine.scale(None,minibatch_scale):
             with pyro.plate("rank_pheno_plate",size=numSamples):
@@ -50,7 +51,7 @@ class VAE(nn.Module):
                 pyro.sample("rankPhenotypes",dist.Normal(z_mean, z_std).to_event(1),obs=latent_vals)
 
 
-    def _encoder_only_guide(self,obs_data=None, cat_cov_list=None,label_dx=None, sample_scores=None,numSamples=None,minibatch_scale=1.0):
+    def _encoder_only_guide(self,obs_data=None, cat_cov_list=None,label_dx=None, sample_scores=None,numSamples=None,minibatch_scale=1.0,annealing_factor=1.0):
         assert sample_scores is not None, "Sample scores must be included in order to train the encoder individually."
         assert obs_data is not None, "obs_data must be included when using the _encoder_only_model."
         
@@ -75,7 +76,7 @@ class VAE(nn.Module):
             if linkFunction=='Logit':
                 self.linkFunction = lambda x:torch.sigmoid(x)
             else:
-                self.linkFunction = lambda x:dist.Normal(0.0,1.0).cdf(x)
+                self.linkFunction = lambda x:dist.Normal(torch.tensor(0.0,dtype=torch.float32,device=x.device),torch.tensor(1.0,dtype=torch.float32,device=x.device)).cdf(x)
         
         if 'computeDevice' not in allKeywordArgs:
             """
@@ -110,7 +111,7 @@ class VAE(nn.Module):
         
             
         if 'encoderNetworkHyperparameters' not in allKeywordArgs:
-            self.encoderHyperparameters={'n_layers' : 2, 'n_hidden' : 64, 'dropout_rate': 0.2, 'use_batch_norm':True}
+            self.encoderHyperparameters={'n_layers' : 2, 'n_hidden' : 64, 'dropout_rate': 0.0, 'use_batch_norm':True}
 
         else:
             self.encoderHyperparameters = kwargs['encoderNetworkHyperparameters']
@@ -118,7 +119,7 @@ class VAE(nn.Module):
             assert set(self.encoderHyperparameters.keys())==set(['n_layers','n_hidden','dropout_rate','use_batch_norm']),"Encoder hyperparameters must include: 'n_layers','n_hidden','dropout_rate','use_batch_norm'"
             
         if 'decoderNetworkHyperparameters' not in allKeywordArgs:
-            self.decoderHyperparameters={'n_layers' : 2, 'n_hidden' : 32, 'dropout_rate': 0.1, 'use_batch_norm':True}
+            self.decoderHyperparameters={'n_layers' : 2, 'n_hidden' : 32, 'dropout_rate': 0.0, 'use_batch_norm':True}
 
         else:
             self.decoderHyperparameters = kwargs['decoderNetworkHyperparameters']
@@ -148,7 +149,7 @@ class VAE(nn.Module):
             self.SwitchDevice(self.compute_device)
         self.eval()
         
-    def model(self, obs_data=None, cat_cov_list=None,label_dx=None,sample_scores=None,numSamples=None,minibatch_scale=1.0):
+    def model(self, obs_data=None, cat_cov_list=None,label_dx=None,sample_scores=None,numSamples=None,minibatch_scale=1.0, annealing_factor=1.0):
         if obs_data is not None:
             numSamples=obs_data.shape[0]
         else:
@@ -165,14 +166,15 @@ class VAE(nn.Module):
         
         with pyro.poutine.scale(None,minibatch_scale):
             with pyro.plate("latent_pheno_plate",size=numSamples):
-                latentPhenotypes=pyro.sample("latentPhenotypes",dist.Normal(torch.zeros(1,self.nLatentDim,dtype=torch.float32,device=self.compute_device),torch.ones(1,self.nLatentDim,dtype=torch.float32,device=self.compute_device)).to_event(1))
+                with pyro.poutine.scale(None, annealing_factor):
+                    latentPhenotypes=pyro.sample("latentPhenotypes",dist.Normal(torch.zeros(1,self.nLatentDim,dtype=torch.float32,device=self.compute_device),torch.ones(1,self.nLatentDim,dtype=torch.float32,device=self.compute_device)).to_event(1))
                 
                 liability_vals = self.decoder.forward(latentPhenotypes,*cat_cov_list)
                 latent_dx_prob = self.linkFunction(liability_vals)
                 pyro.sample("obsTraitIncidence",dist.Bernoulli(latent_dx_prob).to_event(1),obs=obs_data)
 
 
-    def guide(self,obs_data=None, cat_cov_list=None,label_dx=None,sample_scores=None,numSamples = None,minibatch_scale=1.0):
+    def guide(self,obs_data=None, cat_cov_list=None,label_dx=None,sample_scores=None,numSamples = None,minibatch_scale=1.0,annealing_factor=1.0):
         if obs_data is not None:
             numSamples=obs_data.shape[0]
 
@@ -186,7 +188,8 @@ class VAE(nn.Module):
         with pyro.poutine.scale(None,minibatch_scale):
             with pyro.plate("latent_pheno_plate",size=numSamples):
                 z_mean,z_std = self.encoder(obs_data,*cat_cov_list)
-                pyro.sample("latentPhenotypes", dist.Normal(z_mean, z_std).to_event(1))
+                with pyro.poutine.scale(None, annealing_factor):
+                    pyro.sample("latentPhenotypes", dist.Normal(z_mean, z_std).to_event(1))
                 
     
 
@@ -194,7 +197,7 @@ class VAE(nn.Module):
         elboFunc = Trace_ELBO(num_particles=num_particles)
         elboVec = torch.zeros(obs_dis_array.shape[0],dtype=torch.float32,device=self.compute_device)
 
-        for model_trace, guide_trace in elboFunc._get_traces(self.model, self.guide,obs_dis_array,cat_cov_list):
+        for model_trace, guide_trace in elboFunc._get_traces(self.model, self.guide,(obs_dis_array,cat_cov_list),{}):
             elboVec+=model_trace.nodes['obsTraitIncidence']['log_prob'].detach()/num_particles
             elboVec+=model_trace.nodes['latentPhenotypes']['log_prob'].detach()/num_particles
             elboVec-=guide_trace.nodes['latentPhenotypes']['log_prob'].detach()/num_particles

vlpi/optim/Optimizer.py
@@ -10,20 +10,46 @@ import pyro
 from pyro.infer import SVI
 from pyro.infer import Trace_ELBO
 from torch.utils import data
-from pyro.optim import CosineAnnealingWarmRestarts,ReduceLROnPlateau
+from pyro.optim import OneCycleLR
+from torch.optim import AdamW
 from collections import deque
 import numpy as np
 from scipy.stats import linregress
 from pyro import poutine
-from vlpi.optim.AdamW import AdamW
 from vlpi.data.ClinicalDataset import _TorchDatasetWrapper
 from vlpi.utils.UtilityFunctions import rel_diff
 
 
+class AnnealingScheduler:
+    def _cosine_anneal(self,currentStepFrac):
+        cos_vals = np.cos(np.pi * currentStepFrac) + 1.0
+        return self.finalTemp + ((self.initTemp - self.finalTemp) / 2.0) * cos_vals
+    
+    def _linear_anneal(self,currentStepFrac):
+        return (self.finalTemp - self.initTemp) * currentStepFrac + self.initTemp
+    
+    def __init__(self,initTemp,finalTemp,totalNumSteps,scheduleType):
+        assert initTemp>=0.0 and finalTemp >=0.0,"Annealing temperatures must be greater than 0.0"
+        self.initTemp=initTemp
+        self.finalTemp=finalTemp
+        self.totalNumSteps=totalNumSteps
+        if scheduleType=='linear':
+            self._anneal_func=self._linear_anneal
+        else:
+            self._anneal_func=self._cosine_anneal
+        
+    def CurrentTemp(self,currentStep):
+        frac = currentStep/self.totalNumSteps
+        if frac>1.0:
+            return self.finalTemp
+        else:
+            return self._anneal_func(frac)
+        
+
 class Optimizer:
         
     def _returnELBOSlope(self,elbo_deque):
-        return abs(linregress(range(len(elbo_deque)),elbo_deque)[0])/np.mean(elbo_deque)
+        return abs(linregress(range(len(elbo_deque)),elbo_deque)[0]/np.mean(elbo_deque))
         
     def _localShuffle(self,dataSample):
         """
@@ -70,13 +96,12 @@ class Optimizer:
         
         
 
-    def __init__(self,phenotypeModel,datasetSampler,optimizationParameters={'initialLearningRate': 0.05,'maxEpochs': 5000,'numParticles':10},computeConfiguration={'device':None,'numDataLoaders':0},cosineAnnealing={'withCosineAnnealing':False,'initialRestartInterval':10,'intervalMultiplier':2}, **kwargs):
+    def __init__(self,phenotypeModel,datasetSampler,optimizationParameters={'maxLearningRate': 0.05,'maxEpochs': 5000,'numParticles':10},computeConfiguration={'device':None,'numDataLoaders':0}, **kwargs):
         """
         This class implements two SGD optimizers, one of which uses the full training
         dataset (FullDatasetTrain) and the other uses mini-batches randomly sampled
         from the training dataset. To improve convergence rate and escape local minima, 
         learning rate can be autommatically altered during inference using
-        Cosine Annealing with Warm Restarts (see Loshchilov & Hutter 2017).
         
         Note, given stochastic inference strategy coupled with possible re-starts,
         class tracks model perfomance and stores the best possible instance of the model
@@ -84,9 +109,8 @@ class Optimizer:
 
         phenotypeModel-->The phenotype model to optimize
         datasetSampler-->ClinicalDatasetSampler that holds clinical data and generates random samples
-        withCosineAnnealing-->whether to use Cosine Annealing to adjust learning rate during inference. Defaults to scheduler = ReduceLROnPlateau otherwise.
  
-        maxLearningRate-->maximum learning rate used by SGD during inference. If withCosineAnnealing = False, then optimization is initiated with this learning rate once, which is adjusted per LR reduction on plateau with faily strict reduction policy
+        maxLearningRate-->maximum learning rate used by SGD during inference. 
         device-->compute device to use. By default, uses CPU unless GPU device specified, which can be string or integer
     
         """
@@ -98,7 +122,7 @@ class Optimizer:
         
         #general optimization parameters
         self.maxEpochs=optimizationParameters['maxEpochs']
-        self.learningRate = optimizationParameters['initialLearningRate']
+        self.maxLearningRate = optimizationParameters['maxLearningRate']
         self.numParticles = optimizationParameters['numParticles']
         
         #compute resources parameters
@@ -117,23 +141,31 @@ class Optimizer:
 
         self.num_dataloaders=computeConfiguration['numDataLoaders']
         
-        #cosine annealing parameters
         
-        self.withCosineAnnealing = cosineAnnealing['withCosineAnnealing']
-        self.cosineAnnealingParameters = [cosineAnnealing['initialRestartInterval'],cosineAnnealing['intervalMultiplier']]
         
-        if 'AdamW_weight_decay' in allKeywordArgs:
-            self.AdamW_weight_decay=kwargs['AdamW_weight_decay']
+        
+        if 'OneCycleParams' not in allKeywordArgs:
+            self.OneCycleParams={'pctCycleIncrease':0.3,'initLRDivisionFactor':10.0,'finalLRDivisionFactor':1e4}
+
         else:
-            self.AdamW_weight_decay=1e-4
+            self.OneCycleParams=kwargs['OneCycleParams']
+        
+        if 'AdamW_Weight_Decay' in allKeywordArgs:
+            self.AdamW_Weight_Decay=kwargs['AdamW_Weight_Decay']
+        else:
+            self.AdamW_Weight_Decay=1e-4
             
-        if 'LRPlateauTol' in allKeywordArgs:
-            self.LRPlateauTol=kwargs['LRPlateauTol']
+        if 'KLAnnealingParams' not in allKeywordArgs:
+            self.KLAnnealingParams = {'initialTemp':1.0,'maxTemp':1.0,'fractionalDuration':1.0,'schedule': 'cosine'}
         else:
-            self.LRPlateauTol=0.01
+            self.KLAnnealingParams=kwargs['KLAnnealingParams']
+            assert set(self.KLAnnealingParams.keys())==set(['initialTemp','maxTemp','fractionalDuration','schedule']),"KL Annealing Parameters must be dictionary with the following keys: 'initialTemp','maxTemp','fractionalDuration','schedule'"
+            
+        
+            
         
 
-    def _contstructSVI(self,optimizationStrategy):
+    def _contstructSVI(self,optimizationStrategy,total_steps):
         """
         Constructs a list of SVI functions use to train model. Takes optimizationStrategy as input, which must be in ['Full','PosteriorOnly','GuideOnly']
         """
@@ -151,7 +183,6 @@ class Optimizer:
             test_data = self.datasetSampler.GenerateRandomTrainingSample(2)
             if self.useCuda:
                 test_data=self._sendDataToGPU(test_data)
-        
             self.phenotypeModel.guide(*test_data)
             param_store=pyro.get_param_store()
             if optimizationStrategy=='DecoderOnly':
@@ -162,19 +193,11 @@ class Optimizer:
                 _model=poutine.block(self.phenotypeModel.model,hide=[x for x in param_store.get_all_param_names() if ('decoder' in x)])
                 _guide=self.phenotypeModel.guide
             
-        
-        
-            
         #initialize the SVI class
-        AdamWOptimArgs = {'lr': self.learningRate,'weight_decay':self.AdamW_weight_decay}
-        if self.withCosineAnnealing:
-            scheduler = CosineAnnealingWarmRestarts({'optimizer': AdamW, 'optim_args': AdamWOptimArgs, 'T_0': self.cosineAnnealingParameters[0], 'T_mult': self.cosineAnnealingParameters[1]})
-            return {'svi':SVI(_model,_guide,scheduler, loss=Trace_ELBO(num_particles=self.numParticles)),'scheduler':scheduler}
-                
-        else:  
-            scheduler = ReduceLROnPlateau({'optimizer': AdamW, 'optim_args': AdamWOptimArgs, 'threshold': self.LRPlateauTol})
-            return {'svi':SVI(_model,_guide,scheduler, loss=Trace_ELBO(num_particles=self.numParticles)),'scheduler':scheduler}
+        AdamWOptimArgs = {'weight_decay':self.AdamW_Weight_Decay}
         
+        scheduler = OneCycleLR({'optimizer': AdamW, 'optim_args': AdamWOptimArgs,'max_lr':self.maxLearningRate,'total_steps':total_steps,'pct_start':self.OneCycleParams['pctCycleIncrease'],'div_factor':self.OneCycleParams['initLRDivisionFactor'],'final_div_factor':self.OneCycleParams['finalLRDivisionFactor']})
+        return {'svi':SVI(_model,_guide,scheduler, loss=Trace_ELBO(num_particles=self.numParticles)),'scheduler':scheduler}
         
 
     def FullDatasetTrain(self, errorTol:float = 1e-5,verbose:bool=True,logFile=None,optimizationStrategy='Full',errorComputationWindow=None):
@@ -200,6 +223,7 @@ class Optimizer:
         
         if self.useCuda:
             self.phenotypeModel.SwitchDevice(self.device)
+            
         self.phenotypeModel.train()
         
         #training and test data are fixed for full dataset training; therefore, just need
@@ -214,32 +238,32 @@ class Optimizer:
         
         
         assert optimizationStrategy in ['ScoreBased','Full','EncoderOnly','DecoderOnly'],"Available Optimization strategies: ['ScoreBased','Full','EncoderOnly','DecoderOnly']"
-        sviFunction = self._contstructSVI(optimizationStrategy)
+        sviFunction = self._contstructSVI(optimizationStrategy,self.maxEpochs)
+        annealer =AnnealingScheduler(self.KLAnnealingParams['initialTemp'],self.KLAnnealingParams['maxTemp'],int(self.maxEpochs*self.KLAnnealingParams['fractionalDuration']),self.KLAnnealingParams['schedule'])
+        paramUpdateNum = 0
         
-        sviFunction = self._contstructSVI(optimizationStrategy)
         
         # initialize model and parameter storage vectors. Note, parameter storage vectors are stored in a list with each element corresponding to one SVI function
         bestModelState= self.phenotypeModel.PackageCurrentState()
-        bestModelScore = sviFunction['svi'].evaluate_loss(*testData)
-        prev_train_loss = sviFunction['svi'].evaluate_loss(*trainData)
-    
+        bestModelScore = sviFunction['svi'].evaluate_loss(*testData,annealing_factor=annealer.CurrentTemp(paramUpdateNum))
+        prev_train_loss = sviFunction['svi'].evaluate_loss(*trainData,annealing_factor=self.KLAnnealingParams['maxTemp'])
+        elbo_window.append(prev_train_loss)
         
         for epoch in range(self.maxEpochs):
             self._localShuffle(trainData)
             
             # take step on training data
-            currrent_train_loss=sviFunction['svi'].step(*trainData)
+            
+            paramUpdateNum+=1
+            currrent_train_loss=sviFunction['svi'].step(*trainData,annealing_factor=annealer.CurrentTemp(paramUpdateNum))
                         
             #adjust learning rate per scheduler
-            if self.withCosineAnnealing:
-                sviFunction['scheduler'].step(epoch=epoch)
-            else:
-                sviFunction['scheduler'].step(currrent_train_loss)
+            sviFunction['scheduler'].step(epoch=paramUpdateNum)
             
             
             #no point in shuffling testing data, all observations are independent
             #also, all svi functions should return identical loss, so we arbitrarily choose the latter for loss
-            current_test_loss=sviFunction['svi'].evaluate_loss(*testData)
+            current_test_loss=sviFunction['svi'].evaluate_loss(*testData,annealing_factor=self.KLAnnealingParams['maxTemp'])
             
             if current_test_loss<bestModelScore:
                 bestModelState = self.phenotypeModel.PackageCurrentState()
@@ -255,7 +279,6 @@ class Optimizer:
             avg_error = sum(error_window)/len(error_window)
             med_error = sorted(error_window)[int(0.5*len(error_window))]
             slope_error = self._returnELBOSlope(elbo_window)
-            
             prev_train_loss=currrent_train_loss
             errorVec+=[min([avg_error,med_error,slope_error])]
             
@@ -320,70 +343,76 @@ class Optimizer:
             self.phenotypeModel.SwitchDevice(self.device)
             
         self.phenotypeModel.train()
-
+        
         
         # for batch training, use PyTorch dataset loaders, as they allow threaded queuing of samples, which can greatly improve performance.
+        
+
         torchTrainingData=_TorchDatasetWrapper(self.datasetSampler,self.datasetSampler.trainingDataIndex,batch_size)
         torchTestingData=_TorchDatasetWrapper(self.datasetSampler,self.datasetSampler.testDataIndex,batch_size)
         
-        #note, batch_size handled at the level of _TorchDatasetWrapper, not loader itself.
-        trainingDataLoader = data.DataLoader(torchTrainingData,batch_size=1,shuffle=False,sampler=None,batch_sampler=None,num_workers=self.num_dataloaders,collate_fn=lambda x:x[0],pin_memory=self.useCuda)
-        testingDataLoader = data.DataLoader(torchTestingData,batch_size=1,shuffle=False,sampler=None,batch_sampler=None,num_workers=self.num_dataloaders,collate_fn=lambda x:x[0],pin_memory=self.useCuda)
+        
         
         assert optimizationStrategy in ['ScoreBased','Full','EncoderOnly','DecoderOnly'],"Available Optimization strategies: ['ScoreBased','Full','EncoderOnly','DecoderOnly']"
-        sviFunction = self._contstructSVI(optimizationStrategy)
+        sviFunction = self._contstructSVI(optimizationStrategy,len(torchTrainingData)*self.maxEpochs)
+        annealer=AnnealingScheduler(self.KLAnnealingParams['initialTemp'],self.KLAnnealingParams['maxTemp'],int(len(torchTrainingData)*(self.maxEpochs*self.KLAnnealingParams['fractionalDuration'])),self.KLAnnealingParams['schedule'])
+        paramUpdateNum=0
         
         
         #initialize model states and scores
         bestModelState= self.phenotypeModel.PackageCurrentState()
         
-        avg_epoch_test_loss = 0.0
-        for i,data_batch in enumerate(testingDataLoader):
-            if self.useCuda:
-                data_batch=self._sendDataToGPU(data_batch)
-            avg_epoch_test_loss+=sviFunction['svi'].evaluate_loss(*data_batch,minibatch_scale = (numTotalTestingSamples/data_batch[0].shape[0]))
-            
-        bestModelScore = avg_epoch_test_loss/(i+1)
+        #note, batch_size handled at the level of _TorchDatasetWrapper, not loader itself.
+        trainingDataLoader = data.DataLoader(torchTrainingData,batch_size=1,shuffle=False,sampler=None,batch_sampler=None,num_workers=self.num_dataloaders,collate_fn=lambda x:x[0],pin_memory=self.useCuda)
+        testingDataLoader = data.DataLoader(torchTestingData,batch_size=1,shuffle=False,sampler=None,batch_sampler=None,num_workers=self.num_dataloaders,collate_fn=lambda x:x[0],pin_memory=self.useCuda)
         
         avg_epoch_train_loss = 0.0
         for i,data_batch in enumerate(trainingDataLoader):
             if self.useCuda:
                 data_batch=self._sendDataToGPU(data_batch)
-            avg_epoch_train_loss+=sviFunction['svi'].step(*data_batch,minibatch_scale = (numTotalTrainingSamples/data_batch[0].shape[0]))
+            
+            avg_epoch_train_loss+=sviFunction['svi'].evaluate_loss(*data_batch,minibatch_scale = (numTotalTrainingSamples/data_batch[0].shape[0]),annealing_factor=annealer.CurrentTemp(paramUpdateNum+1))
         prev_train_loss=avg_epoch_train_loss/(i+1)
         
         elbo_window.append(prev_train_loss)
+                
+        avg_epoch_test_loss = 0.0
+    
+        for i,data_batch in enumerate(testingDataLoader):
+            if self.useCuda:
+                data_batch=self._sendDataToGPU(data_batch)
+            avg_epoch_test_loss+=sviFunction['svi'].evaluate_loss(*data_batch,minibatch_scale = (numTotalTestingSamples/data_batch[0].shape[0]),annealing_factor=self.KLAnnealingParams['maxTemp'])
+        bestModelScore = avg_epoch_test_loss/(i+1)
+        
         
         for epoch in range(self.maxEpochs):
             torchTrainingData.shuffle_index()
             avg_epoch_train_loss = 0.0
             for i,data_batch in enumerate(trainingDataLoader):
+                paramUpdateNum+=1
                 if self.useCuda:
                     data_batch=self._sendDataToGPU(data_batch)
-                avg_epoch_train_loss+=sviFunction['svi'].step(*data_batch,minibatch_scale = (numTotalTrainingSamples/data_batch[0].shape[0]))
+                avg_epoch_train_loss+=sviFunction['svi'].step(*data_batch,minibatch_scale = (numTotalTrainingSamples/data_batch[0].shape[0]),annealing_factor=annealer.CurrentTemp(paramUpdateNum))
+                sviFunction['scheduler'].step(epoch=(paramUpdateNum))
+                    
                     
             avg_epoch_train_loss=avg_epoch_train_loss/(i+1.0)
-                
-            
-            
-            if self.withCosineAnnealing:
-                sviFunction['scheduler'].step(epoch=epoch)
-            else:
-                sviFunction['scheduler'].step(avg_epoch_train_loss)
-                
                 
             avg_epoch_test_loss = 0.0
             for i,data_batch in enumerate(testingDataLoader):
                 if self.useCuda:
                     data_batch=self._sendDataToGPU(data_batch)
-                avg_epoch_test_loss+=sviFunction['svi'].evaluate_loss(*data_batch,minibatch_scale = (numTotalTestingSamples/data_batch[0].shape[0]))
+                avg_epoch_test_loss+=sviFunction['svi'].evaluate_loss(*data_batch,minibatch_scale = (numTotalTestingSamples/data_batch[0].shape[0]),annealing_factor=self.KLAnnealingParams['maxTemp'])
             avg_epoch_test_loss=avg_epoch_test_loss/(i+1)
             
-            if avg_epoch_test_loss<bestModelScore:
+            if np.isnan(avg_epoch_test_loss) or np.isnan(avg_epoch_train_loss):
+                print("Warning: NaN detected during inference. Model unlikely to be fully optimized!")
+                break
+            
+            elif avg_epoch_test_loss<bestModelScore:
                 bestModelState = self.phenotypeModel.PackageCurrentState()
                 bestModelScore = avg_epoch_test_loss
             
-                
                 
             #track overall convergence
             trainLoss+=[avg_epoch_train_loss]
@@ -418,8 +447,6 @@ class Optimizer:
                     with open(logFile, "a") as f:
                         f.write("Completed %03d epochs; Current Loss (Train, Test): %.4f, %.4f; Error: %.4e\n"%(epoch+1,trainLoss[-1],testLoss[-1],errorVec[-1][0],min([avg_error,med_error,slope_error])))
                 
-            
-            
                 
         if epoch == (self.maxEpochs-1):
             print('Warning: Optimization did not converge in allotted epochs')

vlpi/utils/UnsupervisedMethods.py
@@ -90,6 +90,9 @@ class PhenotypeDecomposition():
             
         self.phenotype_centroid = np.mean(training_data_scores,axis=0)
         return training_data_scores,self.phenotype_centroid
+    
+    def _KL_Diverg(self,raw_data,pred):
+        return np.sum(raw_data*(np.log(raw_data)-np.log(pred))-raw_data+pred,axis=1)
         
 
     def ComputeScores(self,data_array,covariate_matrix=None):
@@ -105,7 +108,9 @@ class PhenotypeDecomposition():
                 covariate_matrix=sprs.csr_matrix(covariate_matrix)
             embeddings-=self.linearCovariateModel.predict(covariate_matrix)
         euclid_dist_from_centroid = np.sqrt(np.sum((embeddings-self.phenotype_centroid)**2.0,axis=1,keepdims=True))
-        return embeddings,euclid_dist_from_centroid
+        reconScores=self._KL_Diverg(transformed_data,np.dot(embeddings,self.ReturnComponents()))
+        
+        return embeddings,euclid_dist_from_centroid,reconScores
         
     def ReturnComponents(self):
         return self.embed_model.components_
@@ -229,36 +234,39 @@ if __name__=='__main__':
     nObsDis=20
     nSamples=100000
     outlierFrac = 0.001
-    from vlpi.ClinicalDataSimulator import ClinicalDataSimulator
+    from vlpi.data.ClinicalDataSimulator import ClinicalDataSimulator
     
     simulator = ClinicalDataSimulator(nObsDis,nLatentDim,latentPhenotypeEffectsPrior=[1.0,5.0],anchorDxNoisePrior=[0.5,10.0],interceptPriors=[-3.0,1.0],anchorDxThresholdPrior=[0.0001,0.01],numMendelianComponents=numMendelianComponents)
     simData=simulator.GenerateClinicalData(nSamples,outlierFrac)
-    simAnchors = simulator.GenerateAnchoringDx(simData['latent_phenotypes'])
+    simAnchors = simulator.GenerateLabelDx(simData['latent_phenotypes'])
     
     phers=PheRS()
     phers_scores=phers.FitModel(simData['incidence_data'].numpy())
     
-    pr=precision_recall_curve(simAnchors['anchor_dx_data'].numpy(),phers_scores)
+    pr=precision_recall_curve(simAnchors['label_dx_data'].numpy(),phers_scores)
     plt.step(pr[1],pr[0],color='b')
     
     nmf=PhenotypeDecomposition(nFitDim)
     scores,centroid=nmf.FitModel(simData['incidence_data'].numpy())
-    scores,dist = nmf.ComputeScores(simData['incidence_data'].numpy())
+    scores,dist,error = nmf.ComputeScores(simData['incidence_data'].numpy())
     phenotype_components = nmf.ReturnComponents()
     
-    pr=precision_recall_curve(simAnchors['anchor_dx_data'].numpy(),dist)
+    pr=precision_recall_curve(simAnchors['label_dx_data'].numpy(),dist)
     plt.step(pr[1],pr[0],color='r')
     
+    pr=precision_recall_curve(simAnchors['label_dx_data'].numpy(),error)
+    plt.step(pr[1],pr[0],color='m')
+    
 
-    pred_anchors_true = np.sum(simAnchors['model_params']['anchorDxMap'].numpy()*simData['latent_phenotypes'].numpy(),axis=1,keepdims=True)
-    pr=precision_recall_curve(simAnchors['anchor_dx_data'].numpy(),pred_anchors_true)
+    pred_anchors_true = np.sum(simAnchors['model_params']['labelDxMap'].numpy()*simData['latent_phenotypes'].numpy(),axis=1,keepdims=True)
+    pr=precision_recall_curve(simAnchors['label_dx_data'].numpy(),pred_anchors_true)
     plt.step(pr[1],pr[0],color='g')
     
     plt.figure()
     centroid=simData['latent_phenotypes'].numpy().mean(axis=0)
     true_euclid_dist = np.sqrt(np.sum((simData['latent_phenotypes'].numpy()-centroid)**2,axis=1,keepdims=True))
     
-    pred_disease_liability = np.sum(simAnchors['model_params']['anchorDxMap'].numpy()*simData['latent_phenotypes'].numpy(),axis=1,keepdims=True)
+    pred_disease_liability = np.sum(simAnchors['model_params']['labelDxMap'].numpy()*simData['latent_phenotypes'].numpy(),axis=1,keepdims=True)
     
     plt.plot(true_euclid_dist[simData['is_outlier'].numpy()==0],pred_disease_liability[simData['is_outlier'].numpy()==0],'o')
     plt.plot(true_euclid_dist[simData['is_outlier'].numpy()==1],pred_disease_liability[simData['is_outlier'].numpy()==1],'o')
@@ -269,8 +277,8 @@ if __name__=='__main__':
     plt.plot(true_euclid_dist[simData['is_outlier'].numpy()==1],dist[simData['is_outlier'].numpy()==1],'o')
     
     plt.figure()
-    plt.plot(true_euclid_dist[simAnchors['anchor_dx_data'].numpy()==0],pred_disease_liability[simAnchors['anchor_dx_data'].numpy()==0],'o')
-    plt.plot(true_euclid_dist[simAnchors['anchor_dx_data'].numpy()==1],pred_disease_liability[simAnchors['anchor_dx_data'].numpy()==1],'o')
+    plt.plot(true_euclid_dist[simAnchors['label_dx_data'].numpy()==0],pred_disease_liability[simAnchors['label_dx_data'].numpy()==0],'o')
+    plt.plot(true_euclid_dist[simAnchors['label_dx_data'].numpy()==1],pred_disease_liability[simAnchors['label_dx_data'].numpy()==1],'o')
     
 #
     

vlpi/vLPI.py
@@ -13,6 +13,8 @@ from vlpi.optim.Optimizer import Optimizer
 from vlpi.data.ClinicalDataset import ClinicalDatasetSampler,ClinicalDataset
 from vlpi.utils.UnsupervisedMethods import PhenotypeDecomposition
 from vlpi.model.VAE import VAE
+from vlpi.model.Classifier import Classifier as _Classifier
+from sklearn.linear_model import LogisticRegression
 
 class vLPI:
 
@@ -69,9 +71,7 @@ class vLPI:
             alreadyInitialized: boolean, indicates whether the model has been pre-initialized (by copying from previous state). This way initialization procedure can be ignored. Can also use to skip initialization procedure on un-initialized model, at which point it is initialize witht the prior.
 
 
-            learningRate: initial learning rate for the stochastic gradient descent. Default is 0.005.
-
-            withCosineAnealing: boolean, indicates whether to use cosine annealing with warm restarts to adjust learning rate during inference. Default is False.
+            maxLearningRate: Maximum learning rate acheived by SVD.
 
             numParticles: number of particles used to approximate gradient during inference. Default is 10.
 
@@ -94,20 +94,25 @@ class vLPI:
             alreadyInitialized=False
 
 
-        if 'learningRate' in allKeywordArgs:
-            learningRate=kwargs['learningRate']
+        if 'maxLearningRate' in allKeywordArgs:
+            maxLearningRate=kwargs['maxLearningRate']
         else:
-            learningRate=0.04
+            maxLearningRate=0.04
 
-        if 'errorTol' in allKeywordArgs:
-            errorTol=kwargs['errorTol']
+        if 'initErrorTol' in allKeywordArgs:
+            initErrorTol=kwargs['initErrorTol']
         else:
-            errorTol = 1e-4
+            initErrorTol = 1e-4
+            
+        if 'finalErrorTol' in allKeywordArgs:
+            finalErrorTol=kwargs['finalErrorTol']
+        else:
+            finalErrorTol=initErrorTol/100.0
 
         if 'numParticles' in allKeywordArgs:
             numParticles=kwargs['numParticles']
         else:
-            numParticles=10
+            numParticles=1
 
 
         if 'maxEpochs' in allKeywordArgs:
@@ -128,54 +133,62 @@ class vLPI:
         else:
             numDataLoaders=0
 
-        if 'withCosineAnnealing' in allKeywordArgs:
-            withCosineAnealing=kwargs['withCosineAnnealing']
+        if 'OneCycleParams' in allKeywordArgs:
+            OneCycleParams=kwargs['OneCycleParams']
+            assert set(OneCycleParams.keys())==set(['pctCycleIncrease','initLRDivisionFactor','finalLRDivisionFactor']),"One-cycle LR scheduler requires the following parameters:'pctCycleIncrease','initLRDivisionFactor','finalLRDivisionFactor'"
+            
         else:
-            withCosineAnealing=True
+            OneCycleParams={'pctCycleIncrease':0.1,'initLRDivisionFactor':25.0,'finalLRDivisionFactor':1e4}
 
-        cosineAnnealingDict = {'withCosineAnnealing':withCosineAnealing,'initialRestartInterval':10,'intervalMultiplier':2}
-
-
-        optimizer=Optimizer(self.model,self.sampler,optimizationParameters={'initialLearningRate': learningRate,'maxEpochs': maxEpochs,'numParticles':numParticles},computeConfiguration={'device':computeDevice,'numDataLoaders':numDataLoaders},cosineAnnealing=cosineAnnealingDict)
+        if 'KLAnnealingParams' in allKeywordArgs:
+            KLAnnealingParams=kwargs['KLAnnealingParams']
+            assert set(KLAnnealingParams.keys())==set(['initialTemp','maxTemp','fractionalDuration','schedule']),"KL Annealing Parameters must be dictionary with the following keys: 'initialTemp','maxTemp','fractionalDuration','schedule'"
+        else:
+            KLAnnealingParams={'initialTemp':0.0,'maxTemp':1.0,'fractionalDuration':0.25,'schedule': 'cosine'}
+            
+        
         pyro.clear_param_store()
+
+        
 
 
         if alreadyInitialized==False:
             print("Initializing the encoder by training the against ICA-initialized NMF Model.")
-            trainScores,testScores,components = self.ComputeNMF_Embeddings(num_components=self.model.nLatentDim)
+            trainScores,testScores,components = self.ComputeNMF_Embeddings(num_components=self.model.nLatentDim,error_tol=finalErrorTol)
 
             scoreVector = np.concatenate([trainScores,testScores])
             allSampleIndex = np.concatenate([self.sampler.trainingDataIndex,self.sampler.testDataIndex])
             self.sampler.AddScoresToDataset(scoreVector,allSampleIndex)
 
 
-
+            optimizer=Optimizer(self.model,self.sampler,optimizationParameters={'maxLearningRate': maxLearningRate,'maxEpochs': maxEpochs,'numParticles':numParticles},computeConfiguration={'device':computeDevice,'numDataLoaders':numDataLoaders},OneCycleParams=OneCycleParams)
             if batch_size > 0:
-                output=optimizer.BatchTrain(batch_size,errorTol=errorTol,optimizationStrategy='ScoreBased',verbose=verbose)
+                output=optimizer.BatchTrain(batch_size,errorTol=initErrorTol,optimizationStrategy='ScoreBased',verbose=verbose)
             else:
-                output=optimizer.FullDatasetTrain(errorTol=errorTol,optimizationStrategy='ScoreBased',verbose=verbose)
+                output=optimizer.FullDatasetTrain(errorTol=initErrorTol,optimizationStrategy='ScoreBased',verbose=verbose)
 
             self.sampler.RemoveScoresFromDataset()
             print("Initializing the decoder by training against the NMF encoder.")
 
 
             if batch_size > 0:
-                output=optimizer.BatchTrain(batch_size,errorTol=errorTol,optimizationStrategy='DecoderOnly',verbose=verbose)
+                output=optimizer.BatchTrain(batch_size,errorTol=initErrorTol,optimizationStrategy='DecoderOnly',verbose=verbose)
             else:
-                output=optimizer.FullDatasetTrain(errorTol=errorTol,optimizationStrategy='DecoderOnly',verbose=verbose)
+                output=optimizer.FullDatasetTrain(errorTol=initErrorTol,optimizationStrategy='DecoderOnly',verbose=verbose)
             print("Re-initializing the encoder by training against the decoder.")
 
             if batch_size > 0:
-                output=optimizer.BatchTrain(batch_size,errorTol=errorTol,optimizationStrategy='EncoderOnly',verbose=verbose)
+                output=optimizer.BatchTrain(batch_size,errorTol=initErrorTol,optimizationStrategy='EncoderOnly',verbose=verbose)
             else:
-                output=optimizer.FullDatasetTrain(errorTol=errorTol,optimizationStrategy='EncoderOnly',verbose=verbose)
+                output=optimizer.FullDatasetTrain(errorTol=initErrorTol,optimizationStrategy='EncoderOnly',verbose=verbose)
 
             print("Model successfully initialized. ELBO:{0:10.2f}".format(-1.0*output[0]))
         print("Optimizing full model.")
+        optimizer=Optimizer(self.model,self.sampler,optimizationParameters={'maxLearningRate': maxLearningRate,'maxEpochs': maxEpochs,'numParticles':numParticles},computeConfiguration={'device':computeDevice,'numDataLoaders':numDataLoaders},OneCycleParams=OneCycleParams,KLAnnealingParams=KLAnnealingParams)
         if batch_size > 0:
-            output=optimizer.BatchTrain(batch_size,errorTol=errorTol,optimizationStrategy='Full',verbose=verbose)
+            output=optimizer.BatchTrain(batch_size,errorTol=finalErrorTol,optimizationStrategy='Full',verbose=verbose)
         else:
-            output=optimizer.FullDatasetTrain(errorTol=errorTol,optimizationStrategy='Full',verbose=verbose)
+            output=optimizer.FullDatasetTrain(errorTol=finalErrorTol,optimizationStrategy='Full',verbose=verbose)
         print('Inference complete. Final ELBO:{0:10.2f}'.format(-1.0*output[0]))
         return -1.0*output[0]
 
@@ -213,7 +226,7 @@ class vLPI:
 
 
 
-    def ComputeNMF_Embeddings(self,num_components=1,includeCovariates=False):
+    def ComputeNMF_Embeddings(self,num_components=1,includeCovariates=False,**kwargs):
         previousArrayType = self.sampler.returnArrays
         if self.sampler.returnArrays!='Sparse':
             self.sampler.ChangeArrayType('Sparse')
@@ -226,10 +239,10 @@ class vLPI:
             train_cov_matrix=None
             test_cov_matrix=None
 
-        embed=PhenotypeDecomposition(num_components=num_components)
+        embed=PhenotypeDecomposition(num_components=num_components,**kwargs)
 
         trainEmbeddings,centroid=embed.FitModel(sparseTrainingData[0],covariateMatrix=train_cov_matrix)
-        testEmbeddings,dist = embed.ComputeScores(sparseTestingData[0],test_cov_matrix)
+        testEmbeddings,dist,recon_errors = embed.ComputeScores(sparseTestingData[0],test_cov_matrix)
         if previousArrayType!='Sparse':
             self.sampler.ChangeArrayType(previousArrayType)
         return trainEmbeddings,testEmbeddings,embed.ReturnComponents()
@@ -282,18 +295,160 @@ class vLPI:
             with open(fName,'wb') as f:
                 torch.save(model_dict,f)
         return model_dict
+    
+    
+class Classifier:
+    
+    def __init__(self,datasetSampler,observedPhenotypeMap='Linear',**kwargs):
+        """
+        Python class that implements the supervised learning algorithms on clinical data.
+                
+        By default, FitModel trains the linear/non-linear classifier
+        
+        Use ElasticNet to access logistic regression in sklearn. 
+        
+        """
+        
+        
+        self.sampler = datasetSampler
+        assert self.sampler.isConditioned, "ClinicalDatasetSampler must be conditioned on some diagnosis/laber of interest in order to use vlpi.Classifier!"
+        
+        assert observedPhenotypeMap in ['Linear','Linear_Monotonic','Nonlinear','Nonlinear_Monotonic'], "Currently supported observed-to-label phenotype maps include: 'Linear','Linear_Monotonic','Nonlinear','Nonlinear_Monotonic'"
+        
+        
+        self.all_model_kwargs = kwargs
+
+        n_cat_list = [len(self.sampler.currentClinicalDataset.catCovConversionDicts[x]) for x in self.sampler.includedCovariates]
+
+        assert observedPhenotypeMap in ['Linear','Linear_Monotonic','Nonlinear','Nonlinear_Monotonic'], "Currently supported latent-to-observed phenotype maps include: 'Linear','Linear_Monotonic','Nonlinear','Nonlinear_Monotonic'"
+    
+        
+        self.model = _Classifier(self.sampler.currentClinicalDataset.numDxCodes, n_cat_list,observedPhenotypeMap,**kwargs)
+        
+    def ElasticNet(self,verbose=True,**kwargs):
+        
+        linMod = LogisticRegression(penalty='elasticnet',tol=errorTol,verbose=verbose,fit_intercept=True,max_iter=maxFittingIterations,solver='saga',warm_start=True)
+        
+    def FitModel(self,batch_size=0,verbose=True,**kwargs):
+        
+        
+        ######### Parse Keyword Arguments #########
+        allKeywordArgs = list(kwargs.keys())
+
+
+        if 'maxLearningRate' in allKeywordArgs:
+            maxLearningRate=kwargs['maxLearningRate']
+        else:
+            maxLearningRate=0.01
+
+        if 'errorTol' in allKeywordArgs:
+            errorTol=kwargs['errorTol']
+        else:
+            errorTol = 1e-4
+
+
+        if 'numParticles' in allKeywordArgs:
+            numParticles=kwargs['numParticles']
+        else:
+            numParticles=1
+
+
+        if 'maxEpochs' in allKeywordArgs:
+            maxEpochs=kwargs['maxEpochs']
+        else:
+            maxEpochs=200
+
+
+        if 'computeDevice' in allKeywordArgs:
+            computeDevice=kwargs['computeDevice']
+        else:
+            computeDevice=None
+
+        if 'numDataLoaders' in allKeywordArgs:
+            numDataLoaders=kwargs['numDataLoaders']
+            if computeDevice in [None,'cpu']:
+                assert numDataLoaders==0,"Specifying number of dataloaders other than 0 only relevant when using GPU computing"
+        else:
+            numDataLoaders=0
+
+        if 'OneCycleParams' in allKeywordArgs:
+            OneCycleParams=kwargs['OneCycleParams']
+            assert set(OneCycleParams.keys())==set(['pctCycleIncrease','initLRDivisionFactor','finalLRDivisionFactor']),"One-cycle LR scheduler requires the following parameters:'pctCycleIncrease','initLRDivisionFactor','finalLRDivisionFactor'"
+            
+        else:
+            OneCycleParams={'pctCycleIncrease':0.1,'initLRDivisionFactor':100.0,'finalLRDivisionFactor':1e4}
+
+        
+        pyro.clear_param_store()
+        optimizer=Optimizer(self.model,self.sampler,optimizationParameters={'maxLearningRate': maxLearningRate,'maxEpochs': maxEpochs,'numParticles':numParticles},computeConfiguration={'device':computeDevice,'numDataLoaders':numDataLoaders},OneCycleParams=OneCycleParams)
+        
+        if batch_size > 0:
+            output=optimizer.BatchTrain(batch_size,errorTol=errorTol,optimizationStrategy='Full',verbose=verbose)
+        else:
+            output=optimizer.FullDatasetTrain(errorTol=errorTol,optimizationStrategy='Full',verbose=verbose)
+        print('Inference complete. Final ELBO:{0:10.2f}'.format(-1.0*output[0]))
+        return -1.0*output[0]
+    
+    def _readModelFromFile(self,fName):
+        with open(fName,'rb') as f:
+            model_dict = torch.load(f,map_location='cpu')
+        return model_dict
+
+
+    def LoadModel(self,stored_model):
+        if not isinstance(stored_model,dict):
+            assert isinstance(stored_model,str),"Expects file name if not provided with dictionary."
+            stored_model = self._readModelFromFile(stored_model)
+        assert set(stored_model.keys())==set(['model_state','meta_data']),"Model dictionary must contain the following elements: 'model_state','meta_data'"
+
+        #first load meta data, make sure it aligns with structure of current vlpi instance
+
+        self.model = _Classifier(stored_model['meta_data']['numObsTraits'], stored_model['meta_data']['numCatList'],stored_model['meta_data']['observedPhenotypeMap'],**self.all_model_kwargs)
+        self.model.LoadPriorState(stored_model)
+
+
+
+    def PackageModel(self,fName=None):
+        model_dict = self.model.PackageCurrentState()
+        model_dict['meta_data']={}
+        model_dict['meta_data']['numObsTraits']=self.model.numObsTraits
+        model_dict['meta_data']['numCatList']=self.model.numCatList
+        model_dict['meta_data']['observedPhenotypeMap']=self.model.decoderType
+
+        if fName is not None:
+            with open(fName,'wb') as f:
+                torch.save(model_dict,f)
+        return model_dict
+    
+    def PredictTestLabels(self):
+        previousArrayType = self.sampler.returnArrays
+        if self.sampler.returnArrays!='Torch':
+            self.sampler.ChangeArrayType('Torch')
+
+        testingData=self.sampler.ReturnFullTestingDataset(randomize=False)
+
+        test_pred= self.model.PredictLabels(testingData[0],testingData[1])
+
+        if previousArrayType!='Torch':
+            self.sampler.ChangeArrayType(previousArrayType)
+        return test_pred.detach().numpy()
+        
+        
+        
 
 if __name__=='__main__':
 
     from vlpi.data.ClinicalDataSimulator import ClinicalDataSimulator
+    from sklearn.metrics import precision_recall_curve
+    import matplotlib.pyplot as plt
     
     pyro.clear_param_store()
 
-    numSamples = 1000
+    numSamples = 10000
     numAssociatedTraits=20
     nLatentSimDim=4
     nLatentFitDim=4
-    simulateLabels=False
+    simulateLabels=True
     mappingFunction='Linear_Monotonic'
 
     numCovPerClass = [2,3,10]
@@ -301,14 +456,10 @@ if __name__=='__main__':
 #    numCovPerClass = []
 #    covNames = []
 
-
-
-    modelParams={}
-    modelParams['latentPhenotypePriors']={'latentPhenotypeEffectsPrecision':[1,10.0]}
-    modelParams['fixedEffectPriors']={'intercepts':[-2,2.0],'covariates_scale':0.025}
-
-    simulator = ClinicalDataSimulator(numAssociatedTraits,nLatentSimDim,numCovPerClass)
-    simData=simulator.GenerateClinicalData(numSamples,0.01)
+#
+#    simulator = ClinicalDataSimulator(numAssociatedTraits,nLatentSimDim,numCovPerClass,interceptPriors=[-3.0,1.5])
+#    simData=simulator.GenerateClinicalData(numSamples,0.001)
+#    labelData=simulator.GenerateLabelDx(simData['latent_phenotypes'])
 
     clinData = ClinicalDataset()
 
@@ -323,7 +474,7 @@ if __name__=='__main__':
 
 
     if simulateLabels:
-        clinData.LoadFromArrays(torch.cat((simData['incidence_data'],simData['label_dx_data']),dim=-1),simData['covariate_data'],covNames,catCovDicts=None, arrayType = 'Torch')
+        clinData.LoadFromArrays(torch.cat((simData['incidence_data'],labelData['label_dx_data']),dim=-1),simData['covariate_data'],covNames,catCovDicts=None, arrayType = 'Torch')
         clinData.ConditionOnDx([labelDx])
         sampler = ClinicalDatasetSampler(clinData,0.75,returnArrays='Torch',conditionSamplingOnDx=[labelDx])
 
@@ -331,8 +482,14 @@ if __name__=='__main__':
         clinData.LoadFromArrays(simData['incidence_data'],simData['covariate_data'],covNames,catCovDicts=None, arrayType = 'Torch')
         sampler = ClinicalDatasetSampler(clinData,0.75,returnArrays='Torch')
 
-    test_vlpi=vLPI(sampler,nLatentFitDim)
-    output=test_vlpi.FitModel(batch_size=100)
+#    test_classifier=Classifier(sampler)
+#    output=test_classifier.FitModel(batch_size=1000,maxLearningRate=0.04,alreadyInitialized=False)
+#    test_pred = test_classifier.PredictTestLabels()
+#    pr = precision_recall_curve(labelData['label_dx_data'].numpy()[np.concatenate(sampler.testDataIndex)],test_pred)
+#    plt.step(pr[1],pr[0])
+
+    test_vlpi=vLPI(sampler,nLatentFitDim,encoderNetworkHyperparameters={'n_layers' : 2, 'n_hidden' : 64, 'dropout_rate': 0.0, 'use_batch_norm':True})
+    output=test_vlpi.FitModel(batch_size=500,maxLearningRate=0.02,maxEpochs=400,alreadyInitialized=False,initErrorTol=1e-3,finalErrorTol=1e-5,KLAnnealingParams={'initialTemp':0.0,'maxTemp':1.0,'fractionalDuration':0.25,'schedule': 'cosine'})
     nmf_train_scores, nmf_test_scores, nmf_components=test_vlpi.ComputeNMF_Embeddings(nLatentFitDim)
 
     model_train_scores,model_test_scores = test_vlpi.ComputeEmbeddings()
@@ -340,6 +497,9 @@ if __name__=='__main__':
 
     euclid_score = test_vlpi.ComputeOutlierScores()
     perplex_train,perplex_test = test_vlpi.ComputePerplexity()
+    
+    
+
     
 #    test_vlpi.PackageModel('/Users/davidblair/Desktop/tmp.pth')
     